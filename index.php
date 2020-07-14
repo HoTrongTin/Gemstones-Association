07eed92e64f726f1dfe1819af797f2069b92361d
Tin Ho Trong <hotinbk@gmail.com>
2020-07-14 21:36:07 +0700

    Chuyen thanh php

---
+++ index.php
@@ -0,0 +1,127 @@
+
+
+<?php require_once('./component/header.php') ?>
+
+    <!-- Swiper -->
+    <div class="swiper-container">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide">
+          <div class="carouselContainer">
+            <img src="./assets/home/photo-1.jpg" />
+            <div class="jumbotron jumbotron-fluid">
+              <h1 class="display-4">GIA Knowledge Sessions</h1>
+              <p class="lead">
+                The GIA Knowledge Sessions are a series of talks and seminars
+                that engage participants in the most diverse and pressing issues
+                in gemology today, including gemstone origin...
+              </p>
+              <hr class="my-4" />
+              <p>
+                The sessions are free and open to the public. Registration in
+                advance is required to attend.
+              </p>
+              <a class="btn btn-warning btn-lg" href="#" role="button"
+                >Read more</a
+              >
+            </div>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="carouselContainer">
+            <img src="./assets/home/photo-2.jpg" />
+            <div class="jumbotron jumbotron-fluid">
+              <h1 class="display-4">Diamond Traceability Based on Science</h1>
+              <p class="lead">
+                Consumers now, more than ever, have a heightened awareness of
+                sustainably and ethically sourced products. The GIA Diamond
+                Origin Report uses scientific evaluation to provide...
+              </p>
+              <hr class="my-4" />
+              <p>
+                The GIA Diamond Origin Report is only available for diamonds
+                that have gone through GIA Rough Analysis Service before
+                polishing.
+              </p>
+              <a class="btn btn-warning btn-lg" href="#" role="button"
+                >Read more</a
+              >
+            </div>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="carouselContainer">
+            <img src="./assets/home/photo-3.jpg" />
+            <div class="jumbotron jumbotron-fluid">
+              <h1 class="display-4">
+                G&G Announces Most Valuable Article Awards
+              </h1>
+              <p class="lead">
+                Gems & Gemology is pleased to announce the winners of the Dr.
+                Edward J. Gübelin Most Valuable Article Awards for 2019.
+              </p>
+              <hr class="my-4" />
+              <p>
+                Aaron Palke is senior manager of research at GIA in Carlsbad,
+                California. He holds a Ph.D. in geology from Stanford
+                University.
+              </p>
+              <a class="btn btn-warning btn-lg" href="#" role="button"
+                >Read more</a
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Add Pagination -->
+      <div class="swiper-pagination"></div>
+    </div>
+
+    <div>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+      <p>Lorem</p>
+    </div>
+
+    <?php require_once('./component/footer.php') ?>
